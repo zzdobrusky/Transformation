b5242d247b5665d5dc10137c312899063a7c1275
zbynek <zzdobrusky@gmail.com>
2014-09-20 15:14:59 -0600

    Animator class

--- app/src/main/java/edu/utah/cs4962/transformation/TransformView.java
+++ app/src/main/java/edu/utah/cs4962/transformation/TransformView.java
@@ -1,5 +1,8 @@
 package edu.utah.cs4962.transformation;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -7,22 +10,33 @@ import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.graphics.PointF;
+import android.media.Image;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by zbynek on 9/20/2014.
  */
 public class TransformView extends ViewGroup
 {
-    View _transformView = null;
+    //View _transformView = null;
+    ImageView _transformView = null;
+
+    ArrayList<PointF> _points = new ArrayList<PointF>();
     public TransformView(Context context)
     {
         super(context);
         setBackgroundColor(Color.BLUE);
 
-        _transformView = new View(context);
+        _transformView = new ImageView(context);
+        _transformView.setImageResource(R.drawable.ic_launcher);
         _transformView.setBackgroundColor(Color.GREEN);
         _transformView.setLayoutParams(new LayoutParams(100, 100));
         addView(_transformView);
@@ -57,12 +71,53 @@ public class TransformView extends ViewGroup
         float x = event.getX();
         float y = event.getY();
 
-        //_transformView.setX(x);
-        //_transformView.setY(y);
-        _transformView.setTranslationX(x);
-        _transformView.setTranslationY(y);
+        _transformView.setX(x - _transformView.getWidth()/2);
+        _transformView.setY(y - _transformView.getHeight()/2);
+//        _transformView.setTranslationX(x);
+//        _transformView.setTranslationY(y);
 //        _transformView.setTranslationX(x - _transformView.getLeft());
 //        _transformView.setTranslationY(y - _transformView.getTop());
+
+        //_transformView.setRotation(x);
+
+//        _transformView.setScaleX(x/getWidth() * 4.0f);
+//        _transformView.setScaleY(y / getHeight() * 4.0f);
+
+        _points.add(new PointF(x, y));
+
+        if(event.getActionMasked() == MotionEvent.ACTION_UP)
+        {
+            float[] pointsX = new float[_points.size()];
+            float[] pointsY = new float[_points.size()];
+            for (int pointIndex=0; pointIndex < _points.size(); pointIndex++)
+            {
+                pointsX[pointIndex] = _points.get(pointIndex).x - _transformView.getWidth()/2;
+                pointsY[pointIndex] = _points.get(pointIndex).y - _transformView.getHeight()/2;
+            }
+            ObjectAnimator animator = new ObjectAnimator();
+            animator.setTarget(_transformView);
+            //animator.setPropertyName("x");
+            animator.setDuration(4000);
+            animator.setValues(
+                    PropertyValuesHolder.ofFloat("x", pointsX),
+                    PropertyValuesHolder.ofFloat("y", pointsY)
+            );
+            //animator.setFloatValues(_transformView.getX(), _transformView.getX() + 400.0f);
+            animator.setInterpolator(new LinearInterpolator());
+            animator.start();
+
+            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener()
+            {
+                @Override
+                public void onAnimationUpdate(ValueAnimator valueAnimator)
+                {
+                    invalidate();
+                }
+            });
+        }
+
+        if(event.getActionMasked() == MotionEvent.ACTION_DOWN)
+            _points.clear();
 
         invalidate();
 
@@ -86,6 +141,9 @@ public class TransformView extends ViewGroup
         canvas.drawText("y: " + _transformView.getY(), 20, 180, textPaint);
         canvas.drawText("translationX: " + _transformView.getTranslationX(), 20, 210, textPaint);
         canvas.drawText("translationY: " + _transformView.getTranslationY(), 20, 240, textPaint);
+        canvas.drawText("rotation: " + _transformView.getRotation(), 20, 270, textPaint);
+        canvas.drawText("scaleX: " + _transformView.getScaleX(), 20, 300, textPaint);
+        canvas.drawText("scaleY: " + _transformView.getScaleY(), 20, 330, textPaint);
 
         Paint layoutPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         layoutPaint.setColor(Color.CYAN);
@@ -112,5 +170,18 @@ public class TransformView extends ViewGroup
         layoutPath.close();
         canvas.drawPath(layoutPath, layoutPaint);
 
+        if (_points.size() > 0)
+        {
+            Paint polylinePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            polylinePaint.setStyle(Paint.Style.STROKE);
+            polylinePaint.setStrokeWidth(2.0f);
+            polylinePaint.setColor(Color.RED);
+            Path polylinePath = new Path();
+            polylinePath.moveTo(_points.get(0).x, _points.get(0).y);
+            for (PointF point : _points)
+                polylinePath.lineTo(point.x, point.y);
+
+            canvas.drawPath(polylinePath, polylinePaint);
+        }
     }
 }
